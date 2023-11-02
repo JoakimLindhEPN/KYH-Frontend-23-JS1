c4b5d450e876ae7d8e686d6beaeb8b8034d88562
Joakim Lindh <joakim.lindh@epndata.com>
2023-11-02 14:41:34 +0100

    lektion 4

--- lektion-4/2-DOM/4-accordion/script.js
+++ lektion-4/2-DOM/4-accordion/script.js
@@ -1,1 +1,18 @@
 const questions = document.querySelectorAll('.question')
+
+
+questions.forEach(question => {
+  question.addEventListener('click', e => {
+    
+    document.querySelectorAll('.answer').forEach(answer => {
+      answer.classList.remove('open')
+    })
+    // question.nextElementSibling.className = 'open'
+    // question.nextElementSibling.classList.toggle('open')
+    const parent = question.parentElement
+    const answer = parent.querySelector('.answer')
+    answer.classList.add('open')
+
+
+  })
+})
