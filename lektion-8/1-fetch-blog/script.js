3f6da57ddb2798b4e74c326ea8a5308f0ee45102
Joakim Lindh <joakim.lindh@epndata.com>
2023-11-16 15:04:54 +0100

    lektion 8 - tog bort vg delar från lösningen

--- lektion-8/1-fetch-blog/script.js
+++ lektion-8/1-fetch-blog/script.js
@@ -46,7 +46,6 @@ function createPostElement(post) {
 
   const contentDiv = createCustomElement('div', 'content')
   const hotBtn = createCustomElement('i', 'fa-solid fa-fire hot-btn')
-  hotBtn.addEventListener('click', () => updatePost(post))
   const contentTitle = createCustomElement('h2', 'content_title', post.title)
 
   const info = createCustomElement('div', 'info')
@@ -85,28 +84,3 @@ function createCustomElement(type, classList, text) {
 }
 
 
-async function updatePost(post) {
-  const res = await fetch('http://localhost:3000/posts/' + post.id, {
-    method: 'PUT',
-    headers: {
-      'Content-type': 'application/json'
-    },
-    body: JSON.stringify({
-      ...post,
-      hot: !post.hot
-    })
-  })
-
-  console.log(res)
-  if(res.status !== 200) return
-
-  
-  // Den här kommer att hämta alla inlägg från databasen på nytt detta kan medföra kostnader beroende på database och trafik
-  // fetchPosts()
-  
-  const newPost = await res.json()
-  // posts.splice(posts.indexOf(post), 1, newPost)
-  post.hot = newPost.hot
-  
-  renderPosts()
-}
