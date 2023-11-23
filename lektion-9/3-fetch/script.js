fc9ad0ef1e7937d3fb49a543574abed2a0e77b62
Joakim Lindh <joakim.lindh@epndata.com>
2023-11-23 11:02:07 +0100

    lektion 10

--- lektion-9/3-fetch/script.js
+++ lektion-9/3-fetch/script.js
@@ -1,5 +1,7 @@
-const BASE_URL = 'https://jsonplaceholder.typicode.com/users'
+const BASE_URL = 'https://jsonplaceholder.typicode.com/users/'
 const userList = document.querySelector('#user-list')
+
+let users = []
 
 const getUsers = async () => {
   try {
@@ -13,16 +15,10 @@ const getUsers = async () => {
     const data = await res.json()
 
     console.log(data)
+    data.forEach(user => users.push(user))
 
-    userList.innerHTML = ''
-    data.forEach(user => {
-      userList.insertAdjacentHTML('beforeend', `
-      <li class="user bg-light p-2 rounded mb-3">
-        <p class="fs-1 mb-0">${user.name}</p>
-        <p class="fs-6 text-secondary m-0">${user.company.name} - ${user.email}</p>
-      </li>
-      `)
-    })
+    renderUsers()
+
 
   } catch(err) {
     document.body.insertAdjacentHTML('beforeend', `
@@ -40,6 +36,62 @@ const getUsers = async () => {
 
 getUsers()
 
+function renderUsers() {
+  userList.innerHTML = ''
+  users.forEach(user => {
+    userList.insertAdjacentHTML('beforeend', `
+    <li class="user bg-light p-2 rounded mb-3">
+      <p class="fs-1 mb-0">${user.name}</p>
+      <p class="fs-6 text-secondary m-0">${user.company.name} - ${user.email}</p>
+      <button data-user-id="${user.id}" id="remove-${user.id}" class="btn remove-btn"><i class="fa-solid fa-trash"></i></button>
+    </li>
+    `)
+
+    document.querySelector('#remove-' + user.id).addEventListener('click', async () => {
+
+      try {
+        const res = await fetch(BASE_URL + user.id, {
+          method: 'DELETE'
+        })
+  
+        console.log(res) 
+
+        if(res.status !== 200) {
+          throw new Error('Could not delete the user: ' + res.status)
+        }
+
+        // const data = await res.json()
+        // console.log(data)
+        
+        users.splice(users.indexOf(user), 1)
+
+        // users = users.filter(_user => _user.id !== data)
+        renderUsers()
+      } 
+      catch (error) {
+        document.body.insertAdjacentHTML('beforeend', `
+        <div class="pop" id="toast">
+          Something went wrong
+        </div>
+        `)
+        document.querySelector('#toast').addEventListener('animationend', e => {
+          e.target.remove()
+        })
+        console.error(err.message)
+      }
+     
+
+    })
+
+  })
+}
+
+// userList.addEventListener('click', e => {
+//   console.log(e.target.nodeName)
+//   if(e.target.nodeName !== 'BUTTON') return
+
+//   console.log(e.target.dataset.userId)
+// })
 
 const form = document.querySelector('#regForm')
 form.addEventListener('submit', async (e) => {
@@ -70,13 +122,16 @@ form.addEventListener('submit', async (e) => {
     const user = await res.json()
     console.log(user)
 
+    users.unshift(user)
 
-    userList.insertAdjacentHTML('afterbegin', `
-      <li class="user bg-light p-2 rounded mb-3">
-        <p class="fs-1 mb-0">${user.name}</p>
-        <p class="fs-6 text-secondary m-0">${user.company.name} - ${user.email}</p>
-      </li>
-      `)
+    // userList.insertAdjacentHTML('afterbegin', `
+    //   <li class="user bg-light p-2 rounded mb-3">
+    //     <p class="fs-1 mb-0">${user.name}</p>
+    //     <p class="fs-6 text-secondary m-0">${user.company.name} - ${user.email}</p>
+    //   </li>
+    //   `)
+
+    renderUsers()
 
       form.reset()
 
